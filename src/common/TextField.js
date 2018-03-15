ef0f549b3ba2aa618f197667c3b4594afd8518c7
yuli.chandra <yuli.chandra877@gmail.com>
2018-03-15 22:31:48 +0700

    add proptypes, defaultProps

--- src/common/TextField.js
+++ src/common/TextField.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 class TextField extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        const {type,text} = props;
         this.state = {
-            type: "text",
-            text: "",
-            error: "",
+            type: type,
+            text: text,
+            errorText: "",
             placeholder: "Test placeholder",
         }
     }
@@ -15,12 +16,7 @@ class TextField extends React.Component{
     textChanged = (e) => {
         this.setState({
             text: e.target.value,
-            error: e.target.value,
         });
-    }
-
-    componentWillMount(){
-        // this.state.text = this.state.text == "" ? this.state.placeholder : this.state.text;
     }
 
     render(){
@@ -30,12 +26,27 @@ class TextField extends React.Component{
                     type={this.state.type} 
                     value={this.state.text}
                     onChange={this.textChanged}
-                    />
-                <br/>
-                <span>{this.state.error}</span>
+                />
+                <span>
+                    {this.state.errorText}
+                </span>
             </div>
         )
     }
+}
+
+TextField.defaultProps = {
+    text: "Hello",
+    type: "text",
+}
+
+TextField.propTypes = {
+    type: PropTypes.oneOf([
+        "text", 
+        ]),
+    text: PropTypes.string,
+    errorText: PropTypes.string,
+    placeholder: PropTypes.string,
 }
 
 export default TextField;
